9d63c9505e30bff88d446fc71e66abb4d4b90c7b
baisilg <pgbaisil@gmail.com>
2020-06-15 11:55:06 +0530

    make code neatly

--- Simple Dynamic Web Project/src/org/baisil/Controller.java
+++ Simple Dynamic Web Project/src/org/baisil/Controller.java
@@ -7,16 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Controller
- */
+
 @WebServlet("/Controller")
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public Controller() {
         super();
         // TODO Auto-generated constructor stub
